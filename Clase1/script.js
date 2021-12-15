045443b20ad78c53e0ce8d484f75a24c69f8649b
Jorge Andres Padilla <jandrespadilla@gmail.com>
2021-12-15 20:40:07 -0300

    first commit

---
+++ Clase1/script.js
@@ -0,0 +1,9 @@
+let nombre = prompt('Ingrese su nombre');
+
+console.log("Hola " + nombre + " ¡vamos a sumar!");
+
+let suma1 = parseInt(prompt('primer numero'));
+let suma2 = parseInt(prompt('segundo numero'));
+let suma = suma1 + suma2;
+console.log("Tu suma dio " + suma);
+alert("Tu suma dio " + suma);
---
+++ clase2/script.js
@@ -0,0 +1,5 @@
+let suma1 = parseInt(prompt('primer numero'));
+let suma2 = parseInt(prompt('segundo numero'));
+let suma = suma1 + suma2;
+
+alert(suma);
