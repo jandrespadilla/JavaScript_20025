78d08e7d698133f8f0ac849efe4fd533b42e7bb6
Jorge Andres Padilla <jandrespadilla@gmail.com>
2021-12-15 20:51:14 -0300

    Comentarios

--- Desafio1/script.js
+++ Desafio1/script.js
@@ -1,9 +1,12 @@
+// Pedimos el nombre 
 let nombre = prompt('Ingrese su nombre');
-
+// Saludamos y damos mensaje motivador
 console.log("Hola " + nombre + " ¡vamos a sumar!");
-
+// Pedimos parametros
 let suma1 = parseInt(prompt('primer numero'));
 let suma2 = parseInt(prompt('segundo numero'));
+// Sumamos
 let suma = suma1 + suma2;
+// Mostramos resultados
 console.log("Tu suma dio " + suma);
 alert("Tu suma dio " + suma);
