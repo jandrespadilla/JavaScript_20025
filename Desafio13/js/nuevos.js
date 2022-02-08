a5c82cd7d55cfb3d8f668ca94aee79dc7c213d38
Jorge Andres Padilla <jandrespadilla@gmail.com>
2022-02-07 22:36:15 -0300

    Desafio 13 animaciones con jquery

--- Desafio13/js/nuevos.js
+++ Desafio13/js/nuevos.js
@@ -1,24 +1,24 @@
-async function buscarPelis(idioma) {
+async function proximamente(idioma) {
     let filmografiaObj=  $.ajax(
-        "https://api.themoviedb.org/3/trending/movie/week?api_key=5e5fc3b9e60f1572acb749241e477ec9&language="+idioma,
+        'https://api.themoviedb.org/3/movie/upcoming?api_key=5e5fc3b9e60f1572acb749241e477ec9&language='+idioma+'&page=1',
         {   async : false, 
             success: function(data) {
-               
-                $("#galeria").slideUp("slow", function(){ 
-                $('#galeria').html('');
+               // return data;
+                $("#proximamente").slideUp("slow", function(){
+                $('#proximamente').html('');
                 indice=0;
                 for (let proxima of data.results) {
                     indice++;
-                    $('#galeria').append('<div id="columna'+indice+'" class="col-4 py-3"></div>');
+                    $('#proximamente').append('<div id="columna'+indice+'" class="col-4 py-3"></div>');
                     $('#columna'+indice).append('<div id="card'+indice+'" class="card"></div>');
                     $('#card'+indice).append('<a id="link'+indice+'" href="./pelicula.html?idPeli='+proxima.id+'&lang='+idioma+'"></a>');
                     $('#link'+indice).append('<img class="card-img-top img_card" src="https://image.tmdb.org/t/p/w300/'+proxima.poster_path+'">');
                     $('#card'+indice).append('<div id="card_body'+indice+'" class="card-body body_card" ></div>');
                     $('#card_body'+indice).append('<h3 class="text-center text-white bg-dark p-1" >'+proxima.title+'</h3>');
                     $('#card_body'+indice).append('<p class="card-text text-dark" >'+proxima.overview+'</p>');
-                }     
-                $("#galeria").slideDown(1000);
-                 });              
+                }   
+                $("#proximamente").slideDown(2000);
+              });                               
             },
             error: function() {
               alert('Hubo un error al cargar los datos los proximos estrenos.');
@@ -26,37 +26,31 @@ async function buscarPelis(idioma) {
          }
       ); 
   }
+ 
+proximamente('es');
 
-
-  async function buscarActores(id,actoresArmados,idioma) {
-
+// Busco actores, todavia no la implemente en la card pero despues voy a incorporarla con mas detalles de la peli
+async function buscarActores(id,actoresArmados) {
     // Traigo actores
-    let response = await fetch("https://api.themoviedb.org/3/movie/"+id+"/credits?api_key=5e5fc3b9e60f1572acb749241e477ec9&language="+idioma)
+    let response = await fetch("https://api.themoviedb.org/3/movie/"+id+"/credits?api_key=5e5fc3b9e60f1572acb749241e477ec9&language=es")
             .then( response => response.json() )
             .then( json => {
-                
                 return json;
             });
-
       actores = await response;
       for (let index = 0; index < 5; index++) {
         const act = actores.cast[index];
         actoresArmados.push(act.name);
-       
     }
-     
       return actoresArmados;
   }
-  
- buscarPelis('es');
-
- $(function() {
+  $(function() {
     $('#toggle-event').change(function() {
           if ($(this).prop('checked')) {
-            buscarPelis('es');
+            proximamente('es');
             loadMenu('es');
         }else{
-            buscarPelis('en');
+            proximamente('en');
             loadMenu('en');
         }
      })
---
+++ Desafio13/js/series.js
@@ -0,0 +1,37 @@
+async function buscarSeries(idioma) {
+    // Traigo series
+    let response = await fetch("https://api.themoviedb.org/3/trending/tv/week?api_key=5e5fc3b9e60f1572acb749241e477ec9&language="+idioma)
+            .then( response => response.json() )
+            .then( json => {
+                return json;
+            });
+            $("#series").slideUp("slow", function(){
+            $('#series').html('');
+            indice=0;
+            for (let serie of response.results) {
+                indice++;
+                      $('#series').append('<div id="columna'+indice+'" class="col-4 py-3"></div>');
+                      $('#columna'+indice).append('<div id="card'+indice+'" class="card"></div>');
+                      //Al link lo agrego despues para no tener que meterme en esto primero termino el resto de los datos que me faltan en los demas detalles
+                     // $('#card'+indice).append('<a id="link'+indice+'" href="./pelicula.html?idPeli='+serie.id+'&lang='+idioma+'"></a>');
+                      $('#card'+indice).append('<img class="card-img-top img_card" src="https://image.tmdb.org/t/p/w300/'+serie.poster_path+'">');
+                      $('#card'+indice).append('<div id="card_body'+indice+'" class="card-body body_card" ></div>');
+                      $('#card_body'+indice).append('<h3 class="text-center text-white bg-dark p-1" >'+serie.name+'</h3>');
+                      $('#card_body'+indice).append('<p class="card-text text-dark" >'+serie.overview+'</p>');
+                } 
+                $("#series").slideDown(2000);
+              });             
+  }
+
+buscarSeries('es');
+  $(function() {
+    $('#toggle-event').change(function() {
+          if ($(this).prop('checked')) {
+            buscarSeries('es');
+            loadMenu('es');
+        }else{
+            buscarSeries('en');
+            loadMenu('en');
+        }
+     })
+  })
