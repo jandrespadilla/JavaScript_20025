c4ecce90abde157a8229c5eed7621c7eee584290
Jorge Andres Padilla <jandrespadilla@gmail.com>
2022-02-10 00:11:17 -0300

    menu

--- Desafio14/js/menu.js
+++ Desafio14/js/menu.js
@@ -20,7 +20,7 @@
       
 function loadMenu() {
     idioma= localStorage.getItem('lang');
-
+ 
     $("#menu").fadeOut("slow", function(){
         $('#menu').html('');
         $('#menu').append(' <a id="logoLink" class="navbar-brand" href="./index.html"></a>');
@@ -29,14 +29,14 @@ function loadMenu() {
         $('#btmBurguer').append('<span class="navbar-toggler-icon bg-lignt"></span>');
         $('#menu').append('<div class="collapse navbar-collapse" id="navbarNav"><div>');
         $('#navbarNav').append('<ul id="ulMenu" class="navbar-nav ms-auto"></ul>');
-        for (let item of menu[0][idioma]) {
+        for (let item of menu[0][localStorage.getItem('lang')]) {
             $('#ulMenu').append('<li id="li'+item[0].split(" ").join("")+'" class="nav-item"></li>');
             $('#li'+item[0].split(" ").join(""))  .append('<a class="nav-link text-white texto-menu" href="'+item[1]+'">'+item[0]+'</a>');
         }         
        // $('#menu').append('<input class="me-4" type="search" placeholder="Search" aria-label="Search"><button class="btn btn-outline-success" type="submit">Search</button>');
         $("#menu").fadeIn(1000);
     });
-    if(idioma=='es'){
+    if(localStorage.getItem('lang')=='es'){
         $('#toggle-event').prop('checked',true);
     } 
 
